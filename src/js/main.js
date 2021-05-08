4805a98419a60385f0fa6c2aa738a7fbe0368672
shingoAkaishi <73268846+shingoAkaishi@users.noreply.github.com>
2021-05-08 15:54:09 +0900

    Install Typescript

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,14 @@ import '../css/main.scss';
 import Vue from 'vue';
 import VueApp from './VueApp.vue';
 
+import add from './add.ts';
+
 new Vue({
   el: '#vue-root',
   render: (h) => h(VueApp),
 })
 
+console.log(add(3, 9));
 console.log('webpack!');
 
 my();
--- src/js/reactApp.jsx
+++ src/js/reactApp.jsx
@@ -1,9 +1,14 @@
 import ReactDom from 'react-dom';
 import * as React from 'react';
 
+import Alert from './Alert.tsx';
+
 const App = (props) => {
   return(
-    <div style={{ color: '#000' }}>Hello, React App!</div>
+    <div style={{ color: '#000' }}>
+      Hello, React App!
+      <Alert message="Success!" />
+    </div>
   );
 };
 
