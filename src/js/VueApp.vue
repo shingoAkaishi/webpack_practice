9e675f061ad784082e23291c3d39d8b8805e5e01
shingoAkaishi <73268846+shingoAkaishi@users.noreply.github.com>
2021-05-08 14:59:32 +0900

    ES6, React, Vue, etc...

---
+++ src/js/VueApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+export default {
+  data: () => {
+    console.log('Vue test')
+    return {
+      message: 'Hey, this is vue!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  color: green;
+}
+</style>
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
+import './reactApp.jsx';
 import my from './app.js';
 import '../css/main.scss';
+
+import Vue from 'vue';
+import VueApp from './VueApp.vue';
+
+new Vue({
+  el: '#vue-root',
+  render: (h) => h(VueApp),
+})
 
 console.log('webpack!');
 
